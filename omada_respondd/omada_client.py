8374360c7a4894d1564480fb594081b25ce7272d
Tobias <admin+github@t0biii.de>
2024-05-13 21:55:32 +0200

    fix uptime

--- omada_respondd/omada_client.py
+++ omada_respondd/omada_client.py
@@ -214,8 +214,7 @@ def get_infos():
                             longitude=float(lon),
                             model=ap.get("model", None),
                             firmware=ap.get("version", None),
-                            #uptime=moreAPInfos.get("uptime", None),
-                            uptime=1,
+                            uptime=moreAPInfos.get("uptimeLong", None),
                             contact=snmp.get("contact", None),
                             load_avg=float(
                                 ap.get("sys_stats", {}).get("loadavg_1", 0.0)
