a9d46df0f9572c234737217eaef91817e287b933
Tobias <admin+github@t0biii.de>
2024-05-13 18:20:06 +0200

    move

--- omada_respondd/respondd_client.py
+++ omada_respondd/respondd_client.py
@@ -335,9 +335,9 @@ class ResponddClient:
     def start(self):
         """This method starts the respondd client."""
         self._sock.setsockopt(
-        socket.SOL_SOCKET,
-        socket.SO_BINDTODEVICE,
-        bytes(self._config.interface.encode()),
+            socket.SOL_SOCKET,
+            socket.SO_BINDTODEVICE,
+            bytes(self._config.interface.encode()),
         )
         if self._config.multicast_enabled:
             self._sock.bind(("::", self._config.multicast_port))
